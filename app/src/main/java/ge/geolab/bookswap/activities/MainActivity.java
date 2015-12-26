4247332e53fd12ccd53a98bc3a59f977f65fed59
Dato Alkhanishvili <davit.alkhanishvili02@geolab.edu.ge>
2015-12-27 01:03:25 +0400

    დაემატა ავტომატური ავტორიზაციის ფუნქცია

--- app/src/main/java/ge/geolab/bookswap/activities/MainActivity.java
+++ app/src/main/java/ge/geolab/bookswap/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package ge.geolab.bookswap.activities;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -8,18 +10,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -33,6 +34,7 @@ import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
@@ -46,8 +48,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import ge.geolab.bookswap.R;
+import ge.geolab.bookswap.models.User;
 import ge.geolab.bookswap.views.adapters.MainActivityPagerAdapter;
-import hugo.weaving.DebugLog;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -58,11 +60,14 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
     @Bind(R.id.navigation_view) NavigationView navigationView;
     @Bind(R.id.fab) FloatingActionButton fab;
-
+    CircleImageView userPicture;
+    TextView fbUserNameTextView;
     private MainActivityPagerAdapter ViewPagerAdapter;
     CallbackManager callbackManager;
     AccessToken accessToken;
     AccessTokenTracker accessTokenTracker;
+    Context context=this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
+
 
 
         accessTokenTracker = new AccessTokenTracker() {
@@ -83,28 +88,34 @@ public class MainActivity extends AppCompatActivity {
                 accessToken=currentAccessToken;
             }
         };
-        // If the access token is available already assign it.
-        accessToken = AccessToken.getCurrentAccessToken();
+        accessToken=AccessToken.getCurrentAccessToken();
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         createDrawerButton();
-        View header = navigationView.getHeaderView(0);
-        CircleImageView userpicture= (CircleImageView) header.findViewById(R.id.profile_image);
         drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+
+        View header = navigationView.getHeaderView(0);
+        userPicture= (CircleImageView) header.findViewById(R.id.profile_image);
+        fbUserNameTextView =(TextView) header.findViewById(R.id.username);
+
+
         ViewPagerAdapter = new MainActivityPagerAdapter(getSupportFragmentManager());
-        // Set up the ViewPager with the sections adapter.
         MainActivityViewPager.setAdapter(ViewPagerAdapter);
+
         tabLayout.setupWithViewPager(MainActivityViewPager);
+
         setNavigationMenuItemListeners();
-        Picasso.with(this).load("https://graph.facebook.com/"+Profile.getCurrentProfile().getId()+"/picture?type=large").into(userpicture);
+        setUserUI();
+
     }
 
     @OnClick(R.id.fab)
     public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+
+        if(checkUserLoginStatus()){
+
+
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -128,7 +139,18 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+    private void setUserUI(){
+        if(AccessToken.getCurrentAccessToken()!=null){
+            Picasso.with(this).load("https://graph.facebook.com/"+Profile.getCurrentProfile().getId()+"/picture?type=large").into(userPicture);
+            fbUserNameTextView.setText(Profile.getCurrentProfile().getName());
+            navigationView.getMenu().getItem(0).setTitle(getString(R.string.logout));
+        }
+    }
+    private void clearUserUI(){
+        userPicture.setImageResource(android.R.color.transparent);
+        fbUserNameTextView.setText("");
+        navigationView.getMenu().getItem(0).setTitle(R.string.login);
+    }
     private void createDrawerButton(){
         /**
          * create drawer button
@@ -183,9 +205,7 @@ public class MainActivity extends AppCompatActivity {
 
                     //Replacing the main content with ContentFragment Which is our Inbox View;
                     case R.id.login:
-                        Toast.makeText(getApplicationContext(), "Inbox Selected", Toast.LENGTH_SHORT).show();
-//
-                        setupFacebookLogin();
+                        loginInFacebook();
                         return true;
 
 
@@ -198,68 +218,130 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void setupFacebookLogin(){
-        List<String> permissionNeeds= Arrays.asList("user_photos","email", "user_birthday", "user_friends");
 
-        LoginManager.getInstance().logInWithReadPermissions(
-                this,
-                permissionNeeds);
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
+    boolean logged[]={false};
 
-                        System.out.println("onSuccess");
-                        GraphRequest request = GraphRequest.newMeRequest
-                                (loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
-                                {
-                                    @Override
-                                    public void onCompleted(JSONObject object, GraphResponse response)
-                                    {
-                                        // Application code
-                                        Log.v("LoginActivity", response.toString());
-                                        //System.out.println("Check: " + response.toString());
-                                        try
-                                        {
-                                            String id = object.getString("id");
-                                            String name = object.getString("name");
-                                            String email = object.getString("email");
-                                            String gender = object.getString("gender");
-                                            String birthday = object.getString("birthday");
-                                            System.out.println(id + ", " + name + ", " + email + ", " + gender + ", " + birthday);
-                                        }
-                                        catch (JSONException e)
-                                        {
-                                            e.printStackTrace();
-                                        }
+    private boolean checkUserLoginStatus(){
+        if (AccessToken.getCurrentAccessToken() != null) {
+           /* UploadStateFragment uploadStateFragment = new UploadStateFragment();
+            uploadStateFragment.show(getFragmentManager(),"uploadFileStateFragment");*/
+            logged[0] = true;
+            return true;
+        }
+        else {
+            new AlertDialog.Builder(context)
+                    .setTitle("განცხადების დამატება")
+                    .setMessage("განცხადების დასამატებლად საჭიროა ავტორიზაცია.\nგსურთ გაიაროთ ავტორიზაცია ?")
+                    .setCancelable(false)
+                    .setNegativeButton("არა", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                                    }
-                                });
-                        Bundle parameters = new Bundle();
-                        parameters.putString("fields", "id,name,email,gender, birthday");
-                        request.setParameters(parameters);
-                        request.executeAsync();
-
-
-                }
-                    @Override
-                    public void onCancel() {
-
-                        Log.e("dd","facebook login canceled");
-
-                    }
-
-
-                    @Override
-                    public void onError(FacebookException e) {
-
-
-
-                        Log.e("dd", "facebook login failed error");
-
-                    }
-                });
+                        }
+                    })
+                    .setPositiveButton("კი", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            loginInFacebook();
+                        }
+                    }).show();
+        }
+        return false;
     }
+
+    private void loginInFacebook(){
+
+        accessToken = AccessToken.getCurrentAccessToken();
+        if (accessToken!=null) {
+            new AlertDialog.Builder(context)
+                    .setTitle("")
+                    .setMessage("გსურთ დატოვოთ პროფილი ?")
+                    .setCancelable(false)
+                    .setPositiveButton("კი", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            LoginManager.getInstance().logOut();
+                            accessToken = null;
+                            logged[0] = false;
+                            clearUserUI();
+                            Snackbar.make(fab, getString(R.string.logout_success), Snackbar.LENGTH_LONG)
+                                    .show();
+
+                        }
+                    })
+                    .setNegativeButton("არა", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            logged[0] = true;
+                        }
+                    }).show();
+        } else {
+            callbackManager = CallbackManager.Factory.create();
+            List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "user_friends");
+
+            LoginManager.getInstance().logInWithReadPermissions(
+                    this,
+                    permissionNeeds);
+            LoginManager.getInstance().registerCallback(callbackManager,
+                    new FacebookCallback<LoginResult>() {
+                        @Override
+                        public void onSuccess(LoginResult loginResult) {
+
+                            GraphRequest request = GraphRequest.newMeRequest
+                                    (loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                        @Override
+                                        public void onCompleted(JSONObject object, GraphResponse response) {
+                                            // Application code
+                                            Log.v("LoginActivity", response.toString());
+                                            //System.out.println("Check: " + response.toString());
+                                            try {
+                                                String id = object.getString("id");
+                                                String name = object.getString("name");
+                                                String email = object.getString("email");
+                                                String gender = object.getString("gender");
+                                                String birthday = object.getString("birthday");
+                                                System.out.println(id + ", " + name + ", " + email + ", " + gender + ", " + birthday);
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+
+                                        }
+                                    });
+                            Bundle parameters = new Bundle();
+                            parameters.putString("fields", "id,name,email,gender, birthday");
+                            request.setParameters(parameters);
+                            request.executeAsync();
+                             logged[0]=true;
+                             setUserUI();
+                            Snackbar.make(fab, getString(R.string.login_success), Snackbar.LENGTH_LONG)
+                                    .show();
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                            Log.e("dd", "facebook login canceled");
+
+                        }
+
+
+                        @Override
+                        public void onError(FacebookException e) {
+
+                            Log.e("dd", "facebook login failed error");
+                            if(e.getMessage().equals(getString(R.string.facebook_connection_error))) {
+                                Snackbar.make(fab, getString(R.string.fb_connectiion_error_snackbar_msg), Snackbar.LENGTH_LONG)
+                                        .show();
+                            }
+                            else {
+                                Snackbar.make(fab, getString(R.string.fb_error_snackbar_msg), Snackbar.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+                    });
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent data)
     {

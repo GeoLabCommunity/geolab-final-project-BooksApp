07e2501c3279c3388af57ffdd24bcbe27d5b7e1b
Dato Alkhanishvili <davit.alkhanishvili02@geolab.edu.ge>
2015-12-26 21:24:28 +0400

    დაემატა Facebook API და ავტორიზაცია

---
+++ app/src/main/java/ge/geolab/bookswap/activities/MainActivity.java
@@ -0,0 +1,276 @@
+package ge.geolab.bookswap.activities;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
+import ge.geolab.bookswap.R;
+import ge.geolab.bookswap.views.adapters.MainActivityPagerAdapter;
+import hugo.weaving.DebugLog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.view_pager) ViewPager MainActivityViewPager;
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.tabs) TabLayout tabLayout;
+    @Bind(R.id.drawer_layout) DrawerLayout drawerLayout;
+    @Bind(R.id.navigation_view) NavigationView navigationView;
+    @Bind(R.id.fab) FloatingActionButton fab;
+
+    private MainActivityPagerAdapter ViewPagerAdapter;
+    CallbackManager callbackManager;
+    AccessToken accessToken;
+    AccessTokenTracker accessTokenTracker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+                accessToken=currentAccessToken;
+            }
+        };
+        // If the access token is available already assign it.
+        accessToken = AccessToken.getCurrentAccessToken();
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        createDrawerButton();
+        View header = navigationView.getHeaderView(0);
+        CircleImageView userpicture= (CircleImageView) header.findViewById(R.id.profile_image);
+        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        ViewPagerAdapter = new MainActivityPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        MainActivityViewPager.setAdapter(ViewPagerAdapter);
+        tabLayout.setupWithViewPager(MainActivityViewPager);
+        setNavigationMenuItemListeners();
+        Picasso.with(this).load("https://graph.facebook.com/"+Profile.getCurrentProfile().getId()+"/picture?type=large").into(userpicture);
+    }
+
+    @OnClick(R.id.fab)
+    public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void createDrawerButton(){
+        /**
+         * create drawer button
+         */
+
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                toolbar,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+        mDrawerToggle.syncState();
+        /**end */
+    }
+
+    private void setNavigationMenuItemListeners(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+
+
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.login:
+                        Toast.makeText(getApplicationContext(), "Inbox Selected", Toast.LENGTH_SHORT).show();
+//
+                        setupFacebookLogin();
+                        return true;
+
+
+                    default:
+                        Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+            }
+        });
+
+    }
+    private void setupFacebookLogin(){
+        List<String> permissionNeeds= Arrays.asList("user_photos","email", "user_birthday", "user_friends");
+
+        LoginManager.getInstance().logInWithReadPermissions(
+                this,
+                permissionNeeds);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        System.out.println("onSuccess");
+                        GraphRequest request = GraphRequest.newMeRequest
+                                (loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
+                                {
+                                    @Override
+                                    public void onCompleted(JSONObject object, GraphResponse response)
+                                    {
+                                        // Application code
+                                        Log.v("LoginActivity", response.toString());
+                                        //System.out.println("Check: " + response.toString());
+                                        try
+                                        {
+                                            String id = object.getString("id");
+                                            String name = object.getString("name");
+                                            String email = object.getString("email");
+                                            String gender = object.getString("gender");
+                                            String birthday = object.getString("birthday");
+                                            System.out.println(id + ", " + name + ", " + email + ", " + gender + ", " + birthday);
+                                        }
+                                        catch (JSONException e)
+                                        {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email,gender, birthday");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+
+                }
+                    @Override
+                    public void onCancel() {
+
+                        Log.e("dd","facebook login canceled");
+
+                    }
+
+
+                    @Override
+                    public void onError(FacebookException e) {
+
+
+
+                        Log.e("dd", "facebook login failed error");
+
+                    }
+                });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data)
+    {
+        super.onActivityResult(requestCode, responseCode, data);
+        callbackManager.onActivityResult(requestCode, responseCode, data);
+
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+    }
+
+}
---
+++ app/src/main/java/ge/geolab/bookswap/views/adapters/MainActivityPagerAdapter.java
@@ -0,0 +1,43 @@
+package ge.geolab.bookswap.views.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ge.geolab.bookswap.fragments.BookSwapFragment;
+
+/**
+ * Created by dalkh on 25-Dec-15.
+ */
+public class MainActivityPagerAdapter extends FragmentPagerAdapter {
+
+    public MainActivityPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a BookSwapFragment (defined as a static inner class below).
+        return BookSwapFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "SECTION 1";
+            case 1:
+                return "SECTION 2";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+}
